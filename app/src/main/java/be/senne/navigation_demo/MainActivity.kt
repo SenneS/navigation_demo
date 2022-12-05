6c76531d88108948393e70354561de4928495a29
Senne <senne.de.neef@student.ehb.be>
2022-12-05 22:55:38 +0100

    tabs test

--- app/src/main/java/be/senne/navigation_demo/MainActivity.kt
+++ app/src/main/java/be/senne/navigation_demo/MainActivity.kt
@@ -1,17 +1,27 @@
 package be.senne.navigation_demo
 
 import android.R.attr
+import android.content.res.Resources
 import android.graphics.BitmapFactory
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import be.senne.navigation_demo.compose.createChatsList
 import be.senne.navigation_demo.model.ChatPreviewData
@@ -23,7 +33,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-
+        myResources = resources
 
         setContent {
             Navigation_demoTheme {
@@ -32,17 +42,17 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-
-                    val bitmap = BitmapFactory.decodeResource(resources, R.drawable.profile_image)
-                    var items = ArrayList<ChatPreviewData>()
-                    for(i in 0 until 100) {
-                        items.add(ChatPreviewData("Gebruiker $i", "Bericht $i", bitmap))
-                    }
-                    createChatsList(items)
+                    //createChatsList(items)
+                    createMainScreen()
                 }
             }
         }
     }
+
+    companion object {
+        lateinit var myResources : Resources
+    }
+
 }
 
 fun hexStringToBytes(str : String) : ByteArray {
@@ -52,49 +62,48 @@ fun hexStringToBytes(str : String) : ByteArray {
 
 @Composable
 fun createMainScreen() {
-    val navController = rememberNavController()
-/*
-    NavHost(navController = navController, startDestination = "chatScreen") {
-        composable("chatsScreen") {
-            val myViewModel = viewModel()
-            createChatsScreen()
+    val tabs = listOf("chatsScreen", "contactsScreen", "settingsScreen")
+    val tabIdx = remember { mutableStateOf(0) }
+    Scaffold(topBar = {
+        TabRow(selectedTabIndex = tabIdx.value, Modifier.padding(20.dp)) {
+            tabs.forEachIndexed { index, s ->
+                Tab(selected = index == tabIdx.value, onClick = { tabIdx.value = index }) {
+                    Text(text = s, fontWeight = FontWeight.Bold, fontSize = 16.sp)
+                }
+            }
         }
-
-        composable("contactsScreen") {
-            createContactsScreen()
-        }
-
-        composable("settingsScreen") {
-            createSettingsScreen()
+    }) { padding -> Column(modifier = Modifier.padding(padding)) {
+        when(tabs[tabIdx.value]) {
+            "chatsScreen" -> {
+                createChatsScreen()
+            }
+            "contactsScreen" -> {
+                createContactsScreen()
+            }
+            "settingsScreen" -> {
+                createSettingsScreen()
+            }
         }
     }
-*/
+    }
 }
 
 @Composable
 fun createChatsScreen() {
-
+    val bitmap = BitmapFactory.decodeResource(MainActivity.myResources, R.drawable.profile_image)
+    var items = ArrayList<ChatPreviewData>()
+    for(i in 0 until 100) {
+        items.add(ChatPreviewData("Gebruiker $i", "Bericht $i", bitmap))
+    }
+    createChatsList(list = items)
 }
 
 @Composable
 fun createContactsScreen() {
-
+    Text("ContactsScreen")
 }
 
 @Composable
 fun createSettingsScreen() {
-
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    Navigation_demoTheme {
-        Greeting("Android")
-    }
+    Text("SettingsScreen")
 }
--- app/src/main/java/be/senne/navigation_demo/compose/chatslist.kt
+++ app/src/main/java/be/senne/navigation_demo/compose/chatslist.kt
@@ -2,22 +2,20 @@ package be.senne.navigation_demo.compose
 
 import android.graphics.BitmapFactory
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.unit.dp
 import be.senne.navigation_demo.model.ChatPreviewData
 
 @Composable
 fun createChatsList(list: List<ChatPreviewData>) {
-    LazyColumn(verticalArrangement = Arrangement.spacedBy(10.dp)) {
+    LazyColumn(verticalArrangement = Arrangement.spacedBy(10.dp), modifier = Modifier.fillMaxWidth()) {
         itemsIndexed(list) { index, item ->
             Row() {
                 Surface() {
